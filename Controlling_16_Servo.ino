0d98c44dc9e89f4d59d7916ad51c236567f496e6
Sakshi Negi <77374152+Sakshi-Negi-2001@users.noreply.github.com>
2023-07-09 19:09:34 +0530

    Add files via upload

---
+++ Controlling_16_Servo.ino
@@ -0,0 +1,43 @@
+#include <Wire.h>
+#include <Adafruit_PWMServoDriver.h>
+
+Adafruit_PWMServoDriver pwm = Adafruit_PWMServoDriver();
+int mipw = 650; // Minimum Pulse Width
+int mapw = 2350; // Maximum Pulse Width
+int dpw = 1500; // Default Pulse Width
+int f = 50; // Frequency
+uint8_t servonum = 0;
+
+void setup() 
+{
+  pwm.begin();
+  pwm.setPWMFreq(f);
+}
+
+void loop() 
+{
+  for(int i=0;i<16;i++)
+  {
+    for(int j=-12;j<=220;j++)
+    {
+      pwm.setPWM(i,0,Angle(j));
+    }
+    delay(300);
+  }
+  for(int i=0;i<16;i++)
+  {
+    for(int j=220;j>=-12;j--)
+    {
+      pwm.setPWM(i,0,Angle(j));
+    }
+    delay(300);
+  }
+}
+int Angle(int angle)
+{
+  int pw;
+  int av;
+  pw = map(angle,0,180,mipw,mapw);
+  av = int(float(pw)/1000000*f*4096);
+  return av;
+}
---
+++ Controlling_Two_Servo_With_Joystick.ino
@@ -0,0 +1,27 @@
+#include <Wire.h>
+#include <Adafruit_PWMServoDriver.h>
+Adafruit_PWMServoDriver pwm = Adafruit_PWMServoDriver();
+int mipw = 650; // Minimum Pulse Width
+int mapw = 2350; // Maximum Pulse Width
+int dpw = 1500; // Default Pulse Width
+int f = 50; // Frequency
+uint8_t servonum = 0;
+void setup() 
+{
+  pwm.begin();
+  pinMode(A0,INPUT);
+  pinMode(A1,INPUT);
+  Serial.begin(9600);
+  pwm.setPWMFreq(f);
+}
+void loop() 
+{
+  pwm.setPWM(0,0,Angle((analogRead(A0))/4));
+  pwm.setPWM(1,0,Angle((analogRead(A1))/4));
+}
+int Angle(int angle)
+{
+  int pw = map(angle,0,180,mipw,mapw);
+  int av = int(float(pw)/1000000*f*4096);
+  return av;
+}
